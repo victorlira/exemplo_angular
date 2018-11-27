3eb90bed9822fe9eccfe854122709b719db405a5
Victor Lira <vl@cin.ufpe.br>
2018-11-27 14:42:36 -0300

    some stuff

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCQ2r3CqlK6Wx-2arSX81ovBOPT8cFngL0',
-    authDomain: 'ifpe-angular.firebaseapp.com',
-    databaseURL: 'https://ifpe-angular.firebaseio.com',
-    projectId: 'ifpe-angular',
-    storageBucket: 'ifpe-angular.appspot.com',
-    messagingSenderId: '40667023248'
+    apiKey: '?',
+    authDomain: '?',
+    databaseURL: '?',
+    projectId: '?',
+    storageBucket: '?',
+    messagingSenderId: '?'
   }
 };

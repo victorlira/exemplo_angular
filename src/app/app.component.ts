ec99e5ddf4a22bd7c6d7f9e5e22feb4eaa0b3b76
Victor Lira <vl@cin.ufpe.br>
2018-12-10 15:10:18 -0300

    css menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,19 @@
 import { Component } from '@angular/core';
 
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition
-} from '@angular/animations';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('slideInOut', [
-      state('in', style({
-        overflow: 'hidden',
-        height: '*',
-        width: '280px'
-      })),
-      state('out', style({
-        overflow: 'hidden',
-        width: '80px'
-      })),
-      transition('in => out', animate('400ms ease-in-out')),
-      transition('out => in', animate('400ms ease-in-out'))
-    ])
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
   isOpen: boolean;
-  helpMenuOpen: string;
 
   constructor() {
     this.isOpen = true;
-    this.helpMenuOpen = 'in';
   }
 
-  toggleHelpMenu(): void {
+  toggleMenu(): void {
     this.isOpen = !this.isOpen;
-    this.helpMenuOpen = this.isOpen ? 'in' : 'out';
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCQ2r3CqlK6Wx-2arSX81ovBOPT8cFngL0',
-    authDomain: 'ifpe-angular.firebaseapp.com',
-    databaseURL: 'https://ifpe-angular.firebaseio.com',
-    projectId: 'ifpe-angular',
-    storageBucket: 'ifpe-angular.appspot.com',
-    messagingSenderId: '40667023248'
+    apiKey: '?',
+    authDomain: '?',
+    databaseURL: '?',
+    projectId: '?',
+    storageBucket: '?',
+    messagingSenderId: '?'
   }
 };
 
